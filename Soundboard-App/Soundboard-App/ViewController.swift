31accd97e16e6097b2c04818bba0dc40c0dbf90a
hoffmantim <hoffmant@hilbertk12.org>
2018-05-03 21:52:15 -0500

    got sounds working

--- Soundboard-App/Soundboard-App/ViewController.swift
+++ Soundboard-App/Soundboard-App/ViewController.swift
@@ -7,17 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player : AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func soundBtnWasPressed(_ sender: UIButton) {
+        
+        
+        
+        let url = Bundle.main.url(forResource: sender.titleLabel!.text, withExtension: "wav")!
+    
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else { return }
+            player.prepareToPlay()
+            player.play()
+        } catch let error as Error {
+            print(error)
+        }
     }
 
 
